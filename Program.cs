0ef5676d7d8efe2816ea595dc0620d2fbaa8d1a0
Vincent <vjdestefano500@gmail.com>
2018-10-23 22:36:23 -0400

    learning about C# classes

--- Program.cs
+++ Program.cs
@@ -10,15 +10,32 @@ namespace First_App
 
         static void Main()
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            //var john = new Person();
+            //john.FirstName = "John";
+            //john.LastName = "Smith";
+            //john.Introduce();
 
 
             Calculator calculator = new Calculator();
             var result = calculator.Add(3, 4);
             Console.WriteLine(result);
+
+
+            var firstname = "Vinny";
+            var lastname = "DeStefano";
+
+            var fullname = firstname + " " + lastname;
+            var myFullName = string.Format("My name is  {0} {1}", firstname, lastname);
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formatedNames = string.Join(",", names);
+
+            Console.WriteLine(fullname);
+            Console.WriteLine(myFullName);
+            Console.WriteLine(names);
+            Console.WriteLine(formatedNames);
+
+
+
         }
 
 
@@ -45,7 +62,19 @@ namespace First_App
 
     }
 
-   
+
+
+    public class Arraytest
+    {
+
+        int[] numberArray = new int[10];
+
+        // use the @ for a verbatim string instead of an indexed string
+        string verbatimPath = @"c:\projects\project1\folder1";
+
+
+
+    }
 
 
 
