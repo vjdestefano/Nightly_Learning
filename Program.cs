6aca218569c7a1e46c18c8a2149ad0d571170c78
Vincent <vjdestefano500@gmail.com>
2018-10-23 21:46:09 -0400

    learning about C# overflow

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace First_App
+{
+
+    class Program
+    {
+
+
+       
+
+
+
+        static void Main()
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+
+        }
+
+
+    }
+
+
+    public class Person{
+
+        public string FirstName;
+        public string LastName;
+
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + "" + LastName);
+        }
+
+
+
+        public static int Calcultor(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+
+   
+
+
+
+}
+
