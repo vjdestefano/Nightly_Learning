a83b629d2c9a930ec18d37dbe823b702354131ea
Vincent <vjdestefano500@gmail.com>
2018-10-23 22:56:42 -0400

    sandbox learning is the best!

---
+++ Sandbox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+
+            user.GetUserInfo();
+        }
+    }
+
+    public class User
+    {
+       
+
+        public void GetUserInfo()
+        {
+
+            Console.WriteLine("Please write your first name!");
+            string infoFirst = Console.ReadLine();
+            Console.WriteLine(infoFirst);
+
+            Console.WriteLine("Please write your last name!");
+            string infoLast = Console.ReadLine();
+            Console.WriteLine(infoLast);
+
+
+        }
+
+
+
+    }
+
+
+
+}
