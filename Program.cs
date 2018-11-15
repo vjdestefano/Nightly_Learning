c1429559cef65fcad70820fa41383d2709f8a0fa
Vincent <vjdestefano500@gmail.com>
2018-11-14 20:57:02 -0500

    this

--- Program.cs
+++ Program.cs
@@ -85,6 +85,33 @@ namespace Arrays
 
     }
 
+   
+        class learningAboutClasses
+        {
+            public string Name;
+            public int Id;
+            public List<Order> Orders;
+
+            public learningAboutClasses()
+            {
+                Orders = new List<Order>();
+            }
+
+
+            public learningAboutClasses(int id)
+                : this()
+            {
+                this.Id = id;
+            }
+
+            public learningAboutClasses(int id, string name)
+                : this(id)
+            {
+                this.Name = name;
+
+            }
+
+        }
 
 
 
